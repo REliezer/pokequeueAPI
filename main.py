327d8cfd97dc7a432c371acc658a04a22511ce8b
Rodrigo Fúnes <rodrigofunes@hotmail.es>
2025-07-31 10:22:29 -0600

    API send messages on Queue

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import uvicorn
 import json
 from fastapi import FastAPI
 from utils.database import execute_query_json
-#from controllers.PokeRequestController import insert_pokemon_request, update_pokemon_request, select_pokemon_request, get_all_request
-#from models.PokeRequest import PokemonRequest
+from controllers.PokeRequestController import insert_pokemon_request#, update_pokemon_request, select_pokemon_request, get_all_request
+from models.PokeRequest import PokemonRequest
 from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
@@ -35,10 +35,10 @@ async def version():
 #async def select_all_request():
 #    return await get_all_request()
 #
-#@app.post("/api/request")
-#async def create_request(pokemon_request: PokemonRequest):
-#    return await insert_pokemon_request( pokemon_request )
-#
+@app.post("/api/request")
+async def create_request(pokemon_request: PokemonRequest):
+    return await insert_pokemon_request( pokemon_request )
+
 #@app.put("/api/request")
 #async def update_request(pokemon_request: PokemonRequest):
 #    return await update_pokemon_request( pokemon_request )
